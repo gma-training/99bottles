2a4f7c89b979f47c4e908ef027a5b2d8808b3958
Graham Ashton <graham@effectif.com>
2017-05-20 08:17:34 +0100

    Implemented another verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
   def verse(number)
     <<-EOF
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{number - 1} bottles of beer on the wall.
 EOF
   end
 end
