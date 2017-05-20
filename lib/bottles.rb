5ca6e253116fcb90190b88f34febfaf7195289a0
Graham Ashton <graham@effectif.com>
2017-05-20 08:31:01 +0100

    Implement verse 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,14 +1,35 @@
-class Group < Struct.new(:number)
+class Bottle < Struct.new(:number)
   def to_s
-    (number == 1) ? "#{number} bottle" : "#{number} bottles"
+    case number
+    when 0
+      'no more bottles'
+    when 1
+      '1 bottle'
+    else
+      "#{number} bottles"
+    end
+  end
+
+  def pronoun
+    case number
+    when 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  def next
+    Bottle.new(number - 1)
   end
 end
 
 class Bottles
   def verse(number)
+    bottle = Bottle.new(number)
     <<-EOF
-#{Group.new(number)} of beer on the wall, #{Group.new(number)} of beer.
-Take one down and pass it around, #{Group.new(number - 1)} of beer on the wall.
+#{bottle} of beer on the wall, #{bottle} of beer.
+Take #{bottle.pronoun} down and pass it around, #{bottle.next} of beer on the wall.
 EOF
   end
 end
